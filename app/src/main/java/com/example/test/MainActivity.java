7e85fb7ebb2619ec9c130d4e4f2ed0c5aa0fc53c
snowfvll <etrnvldespvr@gmail.com>
2024-02-16 13:13:44 +0900

    currencies

---
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listViews);
+
+        ArrayList<DataFlags> list = new ArrayList<>();
+        list.clear();
+
+        list.add(new DataFlags(R.drawable.flag_russian, R.string.russian, R.string.russianRUB));
+        list.add(new DataFlags(R.drawable.flag_africa, R.string.africa, R.string.africaZAR));
+        list.add(new DataFlags(R.drawable.flag_singapore, R.string.singapore, R.string.singaporeSGD));
+        list.add(new DataFlags(R.drawable.flag_turkish, R.string.turkish, R.string.turkishTRY));
+        list.add(new DataFlags(R.drawable.flag_usa, R.string.usa, R.string.usaUSD));g
+        list.add(new DataFlags(R.drawable.flag_switzerland, R.string.switzerland, R.string.switzerlandCHF));
+        list.add(new DataFlags(R.drawable.flag_korea, R.string.korea, R.string.koreaKRW));
+
+        MyListAdapter myListAdaptery = new MyListAdapter(this, list);
+        listView.setAdapter(myListAdaptery);
+    }
+}
