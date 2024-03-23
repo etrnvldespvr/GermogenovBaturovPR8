92ac9d9cd7e1f08ef7114e68ef86ae160c5302ab
GermogenovBatyurov <germogenovbatyurov@iMac-Administrator.local>
2024-03-23 10:46:21 +0900

    map

---
+++ app/src/main/java/com/example/germmaps/MapsActivity.java
@@ -0,0 +1,72 @@
+package com.example.germmaps;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.germmaps.databinding.ActivityMapsBinding;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_maps);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            mMap.getUiSettings().setZoomControlsEnabled(true);
+            mMap.getUiSettings().setZoomGesturesEnabled(true);
+            mMap.getUiSettings().setCompassEnabled(true);
+        // Add a marker in Sydney and move the camera
+        LatLng yakutsk = new LatLng(62.03840568481608, 129.73238991924592);
+        mMap.addMarker(new MarkerOptions().position(yakutsk).title("Marker in Yakutsk"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(yakutsk));
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng point) {
+                MarkerOptions marker = new MarkerOptions()
+                    .position(new LatLng(point.latitude, point.longitude))
+                    .title("Координаты: " + point.latitude + " " + point.longitude);
+                    mMap.addMarker(marker);
+                    System.out.println(point.latitude + "---" + point.longitude);
+            }
+        });
+    }
+
+    public void ChangeType(View view)
+    {
+        if (mMap.getMapType()==GoogleMap.MAP_TYPE_NORMAL){
+            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        }
+        else if (mMap.getMapType()==GoogleMap.MAP_TYPE_SATELLITE){
+            mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        }
+        else if (mMap.getMapType()==GoogleMap.MAP_TYPE_HYBRID){
+            mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        }
+        else if (mMap.getMapType()==GoogleMap.MAP_TYPE_TERRAIN){
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        }
+    }
+}
